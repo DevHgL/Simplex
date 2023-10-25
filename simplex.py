bd6b5f7edcd6e8ba11a08158416aca4cd5f127ae
DevHgL <hleomelo1@icloud.com>
2023-10-24 21:04:23 -0300

    fodassi essa porra

--- simplex.py
+++ simplex.py
@@ -1,70 +1,94 @@
-import numpy as np
+import time
 
-def read_input(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
 
-    m, n, maximize = map(int, lines[0].split())
-    c = np.array(list(map(float, lines[1].split())))
-    A = []
-    b = []
+def buscar_variavel_entrada():
+    global variavel_entrada, linha_menor_elemento, coluna_pivo
+    variavel_entrada = 1000
+    linha_menor_elemento = 0
+    coluna_pivo = 0
 
-    for line in lines[2:]:
-        parts = line.split()
-        coefficients = list(map(float, parts[:-1]))
-        if parts[-1] == '<=':
-            coefficients += [0.0] * (m - len(coefficients)) + [1.0]
-        elif parts[-1] == '==':
-            coefficients += [0.0] * (m - len(coefficients)) + [-1.0]
-        else:
-            coefficients += [0.0] * (m - len(coefficients)) + [-1.0]
-            coefficients.append(1.0)
-        A.append(coefficients)
-        b.append(float(parts[-2]) * -1 if parts[-1] == '>=' else float(parts[-2]))
+    output_file.write("\n\nProcurando Variável Que Entra:\n")
+    time.sleep(4)
 
-    return c, np.array(A), np.array(b), maximize
+    for i in range(total_linhas):
+        for j in range(total_colunas):
+            if tabela[i][j] < variavel_entrada:
+                variavel_entrada = tabela[i][j]
+                linha_menor_elemento = i
+                coluna_pivo = j
 
-def simplex(c, A, b, maximize):
-    m, n = A.shape
-    c = np.concatenate([c, np.zeros(m)])
-    A = np.column_stack([A, np.eye(m)])
-    x = np.zeros(n + m)
-    basis = np.arange(n, n + m)
-    obj_value = np.inf
+    output_file.write(f"Assim temos que a Variável que Entra é |{variavel_entrada}|\n")
 
-    while True:
-        reduced_costs = c - np.dot(c[basis], np.linalg.inv(A[:, basis]))
-        if maximize:
-            entering = np.argmax(reduced_costs)
-        else:
-            entering = np.argmin(reduced_costs)
 
-        if reduced_costs[entering] >= 0:
-            break
+def buscar_linha_pivo():
+    global menor_pivo, linha_pivo, elemento_pivo
+    menor_pivo = 1000
+    linha_pivo = 0
+    elemento_pivo = 0
 
-        ratios = b / A[:, entering]
-        ratios[ratios < 0] = np.inf
-        leaving = np.argmin(ratios)
+    output_file.write("\nObtendo Linha Pivo:\n\n")
+    time.sleep(5)
 
-        x[entering], x[basis[leaving]] = ratios[leaving], 0
-        basis[leaving] = entering
+    for i in range(total_linhas):
+        if i == linha_menor_elemento:
+            continue
+        coluna_variavel_entrada = tabela[i][total_colunas - 1] / tabela[i][coluna_pivo]
+        if coluna_variavel_entrada < menor_pivo and coluna_variavel_entrada > 0:
+            menor_pivo = coluna_variavel_entrada
+            linha_pivo = i
+    elemento_pivo = tabela[linha_pivo][coluna_pivo]
+    output_file.write(f"Linha Pivo: {linha_pivo}\n")
+    output_file.write(f"\nElemento Pivo: {elemento_pivo}\n")
 
-        pivot = A[leaving, entering]
-        A[leaving, :] /= pivot
-        b[leaving] /= pivot
 
-        for i in range(m):
-            if i != leaving:
-                factor = A[i, entering]
-                A[i, :] -= factor * A[leaving, :]
-                b[i] -= factor * b[leaving]
-
-        obj_value = np.dot(c[basis], np.linalg.inv(A[:, basis]))
-
-    return obj_value, x[:n]
+# Funções restantes...
 
 if __name__ == "__main__":
-    c, A, b, maximize = read_input("problema1.txt")
-    obj_value, solution = simplex(c, A, b, maximize)
-    print("Objective Value:", obj_value)
-    print("Solution:", solution)
+    input_file = open("problema1.txt", "r")
+    output_file = open("output.txt", "w")
+
+    qtd_x, qtd_funcoes_limitantes, qtd_restricoes = map(int, input_file.readline().split())
+    total_colunas = qtd_x + qtd_funcoes_limitantes + 2
+    total_linhas = qtd_funcoes_limitantes + 1
+
+    output_file.write("Informe os valores da tabela inicial formada linha a linha:\n")
+
+    tabela = []
+
+    # Lendo a função objetiva
+    funcao_objetiva = list(map(float, input_file.readline().split()))
+
+    # Lendo as restrições e adicionando as variáveis de folga e artificiais conforme necessário
+    for i in range(qtd_funcoes_limitantes):
+        restricao = input_file.readline().split()
+        coeficientes = list(map(float, restricao[:-2]))
+        sinal = restricao[-2]
+        termo_independente = float(restricao[-1])
+
+        # Se o termo independente é negativo, multiplicamos toda a linha por -1
+        if termo_independente < 0:
+            coeficientes = [-coef for coef in coeficientes]
+            termo_independente *= -1
+
+        # Adicionando as variáveis de folga e artificiais conforme necessário
+        if sinal == "<=":
+            coeficientes.extend([1] + [0] * (qtd_funcoes_limitantes - 1))
+        elif sinal == "==":
+            coeficientes.extend([0] * qtd_funcoes_limitantes)
+        elif sinal == ">=":
+            coeficientes.extend([-1] + [0] * (qtd_funcoes_limitantes - 1))
+
+        coeficientes.append(termo_independente)
+
+        tabela.append(coeficientes)
+
+    # Adicionando a função objetiva ao final da tabela
+    funcao_objetiva.extend([0] * qtd_funcoes_limitantes)
+    funcao_objetiva.append(0)
+
+    tabela.append(funcao_objetiva)
+
+    # Restante do código...
+
+    input_file.close()
+    output_file.close()
