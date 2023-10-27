94bc0fb2f1570d049c62ead003c9138ad4a676ca
DevHgL <hleomelo1@icloud.com>
2023-10-27 16:54:38 -0300

    commit

--- simplex.py
+++ simplex.py
@@ -1,91 +1,51 @@
-def buscar_variavel_entrada():
-    global variavel_entrada, linha_menor_elemento, coluna_pivo
-    variavel_entrada = 1000
-    linha_menor_elemento = 0
-    coluna_pivo = 0
+import numpy as np
+import sys
+from resolve import resolve
+from cria_tableau import criar_tableau
 
-    for i_novo2 in range(total_linhas):
-        for j in range(total_colunas):
-            if tabela[i_novo2][j] < variavel_entrada:
-                variavel_entrada = tabela[i_novo2][j]
-                linha_menor_elemento = i_novo2
-                coluna_pivo = j
+def arrayToFloat(array):
+    return list( map( lambda val: float(val), array ) )
 
+def arrayToInt(array):
+    return list( map( lambda val: int(val), array ) )
 
-def buscar_linha_pivo():
-    global menor_pivo, linha_pivo, elemento_pivo
-    menor_pivo = 1000
-    linha_pivo = 0
-    elemento_pivo = 0
-
-    for i_novo in range(total_linhas):
-        if i_novo == linha_menor_elemento:
-            continue
-        if tabela[i_novo][coluna_pivo] != 0:
-            coluna_variavel_entrada = tabela[i_novo][total_colunas - 1] / tabela[i_novo][coluna_pivo]
-            if menor_pivo > coluna_variavel_entrada > 0:
-                menor_pivo = coluna_variavel_entrada
-                linha_pivo = i_novo
-    elemento_pivo = tabela[linha_pivo][coluna_pivo]
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        path = sys.argv[1]
+    else:
+        path = str(input("Digite o path do arquivo do PPL: "))
 
 
-# Funções restantes...
+    with open(path, "r") as arq:
+        content = arq.read()
+    lines = content.split('\n')
 
-if __name__ == "__main__":
-    input_file = open("problema1.txt", "r")
+    num_restricoes, num_variaveis, otimizacao = arrayToInt(lines[0].split(' '))
+    coeficientes_funcao_objetivo = arrayToFloat(lines[1].split(' '))
 
-    qtd_x, qtd_funcoes_limitantes, qtd_restricoes = map(int, input_file.readline().split())
-    total_colunas = qtd_x + qtd_funcoes_limitantes + 2
-    total_linhas = qtd_funcoes_limitantes + 1
+    # FO
+    coeficientes_funcao_objetivo = np.array(coeficientes_funcao_objetivo)
 
-    tabela = []
+    coeficientes_restricoes = []
+    operations = []
+    termos_independentes = []
 
-    # Lendo a função objetiva
-    funcao_objetiva = list(map(float, input_file.readline().split()))
+    # Capturando valores
+    for i in range(num_restricoes):
+        *coeficientes_restricao, operation, termo_independente = lines[2+i].split(' ')
+        
+        coeficientes_restricoes.append(arrayToFloat(coeficientes_restricao))
+        operations.append(operation)
+        termos_independentes.append(float(termo_independente))
+    
+    # A
+    coeficientes_restricoes = np.array(coeficientes_restricoes)
+    operations = np.array(operations)
+    # b
+    termos_independentes = np.array(termos_independentes)
 
-    # Lendo as restrições e adicionando as variáveis de folga e artificiais conforme necessário
-    for i in range(qtd_funcoes_limitantes):
-        restricao = input_file.readline().split()
-        coeficientes = list(map(float, restricao[:-2]))
-        sinal = None  # Define um valor padrão para 'sinal'
-        termo_independente = None  # Define um valor padrão para 'termo_independente'
-        if len(restricao) >= 2:
-            sinal = restricao[-2]
-        else:
-            print(f"Formato de linha inesperado: {restricao}")
-        if len(restricao) >= 1:
-            termo_independente = float(restricao[-1])
-            # Se o termo independente é negativo, multiplicamos toda a linha por -1
-            if termo_independente < 0:
-                coeficientes = [-coef for coef in coeficientes]
-                termo_independente *= -1
-        else:
-            print(f"Formato de linha inesperado: {restricao}")
+    tableau = criar_tableau(coeficientes_funcao_objetivo, coeficientes_restricoes, termos_independentes, otimizacao, operations)
+    solved = resolve(np.copy(tableau), otimizacao)
 
-        # Adicionando as variáveis de folga e artificiais conforme necessário
-        if sinal == "<=":
-            coeficientes.extend([1] + [0] * (qtd_funcoes_limitantes - 1))
-        elif sinal == "==":
-            coeficientes.extend([0] * qtd_funcoes_limitantes)
-        elif sinal == ">=":
-            coeficientes.extend([-1] + [0] * (qtd_funcoes_limitantes - 1))
-            coeficientes.append(1)  # Adiciona uma variável artificial
-
-        coeficientes.append(termo_independente)
-
-        tabela.append(coeficientes)
-
-    # Adicionando a função objetiva ao final da tabela
-    funcao_objetiva.extend([0] * qtd_funcoes_limitantes)
-
-    # Se é um problema de maximização, multiplica a função objetivo por -1
-    if qtd_restricoes == 1:
-        funcao_objetiva = [-x for x in funcao_objetiva]
-
-    funcao_objetiva.append(0)
-
-    tabela.append(funcao_objetiva)
-
-    # Restante do código...
-
-    input_file.close()
+    print(solved)
+    print(tableau)
